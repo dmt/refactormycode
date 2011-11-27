9117e02a009f784eefa6ed10ef36a8a4c618a263
Daniel Temme <dtemme@gmail.com>
2011-11-27 17:21:09 +0100

    move assertions into separate tests

--- java/string_calculator/src/test/java/StringCalculatorTest.java
+++ java/string_calculator/src/test/java/StringCalculatorTest.java
@@ -1,16 +1,39 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static junit.framework.Assert.assertEquals;
 
 public class StringCalculatorTest {
 
+    private StringCalculator s;
+
+    @Before
+    public void setUp() throws Exception {
+        s = new StringCalculator();
+    }
+
     @Test
-    public void testCalculator() throws Exception {
-        StringCalculator s = new StringCalculator();
-        assertEquals(0, s.add("")); //should treat empty string as zero
-        assertEquals(17, s.add("17")); // should add single numbers
-        assertEquals(2356, s.add("2341,15")); // should add comma separated numbers
-        assertEquals(35, s.add("34\n1")); // should treat new line as separator
-        assertEquals(7, s.add("//;\n5;2")); // should support custom separators
+    public void treatsEmptyStringsAsZero() {
+        assertEquals(0, s.add(""));
+    }
+
+    @Test
+    public void addsSingleNumbers() {
+        assertEquals(17, s.add("17"));
+    }
+
+    @Test
+    public void addsCommaSeparatedNumbers() {
+        assertEquals(2356, s.add("2341,15"));
+    }
+
+    @Test
+    public void treatsNewLineAsSeparator() {
+        assertEquals(35, s.add("34\n1"));
+    }
+
+    @Test
+    public void supportsCustomSeparators() throws Exception {
+        assertEquals(7, s.add("//;\n5;2"));
     }
 }
